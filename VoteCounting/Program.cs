2e40a788915c49f9c0d88d7e3197f884fb704c9e
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2023-12-15 14:42:42 -0300

    Finalização do projeto

--- VoteCounting/Program.cs
+++ VoteCounting/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VoteCounting {
+
+    class Program {
+        static void Main(string[] args) {
+           
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+                    while (!sr.EndOfStream) {
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int vote = int.Parse( votingRecord[1]);
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += vote;
+                        } else {
+                            dictionary[candidate] = vote;
+                        }
+                        
+                    }
+                    foreach (var item in dictionary) {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }catch(IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
